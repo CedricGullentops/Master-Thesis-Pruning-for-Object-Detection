b8160a75462649fb194a7ab5bffbe4cdd365fa8b
CedricGullentops <cedricgullentops@hotmail.com>
2019-11-07 21:28:06 +0100

    Base pruning almost done

---
+++ docker-omgeving/Code/Pruning.py
@@ -0,0 +1,98 @@
+#
+#   Main pruning class
+#   Simple running example: Python3 Pruning.py 30.0
+#
+
+# Basic imports
+import torch
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import argparse
+import brambox as bb
+import lightnet as ln
+from utils import getnet,getlossfunction,isconvoltionlayer
+from softprune import softprune
+from l2prune import *
+
+# Settings
+ln.logger.setConsoleLevel('ERROR')  # Only show error log messages
+bb.logger.setConsoleLevel('ERROR')  # Only show error log messages
+
+
+class Pruning:
+    def __init__(self, model, method, loss, percentage, manner,  **kwargs):
+        self.method = method
+        self.model = model
+        self.percentage = percentage
+        self.manner = manner
+        self.loss = lossfunction
+
+        # Setting kwargs
+        for k, v in kwargs.items():
+            if not hasattr(self, k):
+                setattr(self, k, v)
+            else:
+                print('{k} attribute already exists, not overwriting with `{v}`')
+
+        self.convolutionlayerlist = self.findconvolutionallayers()
+
+
+    def __call__(self):
+        if self.method == 'l2prune':
+            prune = L2prune(self)
+            prune()
+        if self.method == 'centripetalSGD':
+            prune = L2prune(self)
+            prune()
+        if self.method == 'geometricmedian':
+            prune = L2prune(self)
+            prune()
+        else:
+            'No valid method was chosen, exiting'
+            quit()
+
+
+    def findconvolutionallayers(self):
+        convolutionlayerlist = []
+        for sequential in range(len(self.model.layers)):
+            for layer in range(len(self.model.layers[sequential])):
+                if isconvoltionlayer(self.model, sequential, layer):
+                    convolutionlayerlist.append((sequential,layer))
+        return convolutionlayerlist
+
+
+if __name__ == '__main__':
+    # Define arguments
+    parser = argparse.ArgumentParser(
+        description='Prune a given network for a given percentage',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('percentage', help='Percentage of network to be pruned', type=float)
+    parser.add_argument('-ml', '--maxloss', help='Maximum acceptable loss to accuracy.', type=float)
+    parser.add_argument('-me', '--method', choices=['l2prune', 'centripetalSGD', 'geometricmedian'], default='l2prune',
+                        help='The pruning method that will be used')
+    parser.add_argument('-l', '--loss', help='Loss function to use, default is RegionLoss.',
+                        choices=['RegionLoss'], default='RegionLoss')
+    parser.add_argument('-n', '--network', help='Pretrained network to prune',
+                        choices=['Yolo', 'Yolt', 'DYolo', 'TinyYolo', 'MobileNetYolo'], default='Yolo')
+    parser.add_argument('-m', '--manner', choices=['hard', 'soft', 'combination'], default='hard',
+                        help='The manner in which to prune: soft, hard or a combination of both')
+    parser.add_argument('-o', '--optimizer', help='Optimizer to use')
+    args = parser.parse_args()
+
+    net = getnet(args.network)
+    lossfunction = getlossfunction(args.loss, net)
+
+    # Start pruning
+    prune = Pruning(
+        model=net,
+        method=args.method,
+        loss=lossfunction,
+        percentage=args.percentage,
+        manner=args.manner,
+        optimizer=args.optimizer,
+        maxloss=args.maxloss,
+    )
+    prune()
--- docker-omgeving/Code/l2prune.py
+++ docker-omgeving/Code/l2prune.py
@@ -1,71 +1,92 @@
+#
+#   L2norm pruning
+#   Credits: https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+#   https://github.com/zepx/pytorch-weight-prune
+#
+
 # Basic imports
-import torch
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-import argparse
-import brambox as bb
 import lightnet as ln
-from getnet import getnet
-from hardprune import hardprune
+import numpy as np
+from utils import arg_nonzero_min, printProgressBar
 
 # Settings
 ln.logger.setConsoleLevel('ERROR')  # Only show error log messages
-bb.logger.setConsoleLevel('ERROR')  # Only show error log messages
 
 
 class L2prune:
-    def __init__(self, network, loss, percentage, manner, **kwargs):
-        self.network = network
-        self.loss = loss
-        self.percentage = percentage
-        self.manner = manner
-
-        # Setting kwargs
-        for k, v in kwargs.items():
-            if not hasattr(self, k):
-                setattr(self, k, v)
-            else:
-                log.error('{k} attribute already exists on TestEngine, not overwriting with `{v}`')
-
+    def __init__(self, Pruning):
+        self.Pruning = Pruning
+        self.totalfilters = 0
+        self.prunedfilters = 0
+        for x in Pruning.convolutionlayerlist:
+            self.totalfilters += Pruning.model.layers[x[0]][x[1]].out_channels
+        self.pruneamount = 0.01*Pruning.percentage*self.totalfilters
+        print('There are', self.totalfilters , 'filters in convolutional layers, attempting to prune', int(self.pruneamount), 'filters')
+        
     def __call__(self):
-        print(self.network)
-        if self.manner == 'hard':
-            hardprune(self.network)
-        elif self.manner == 'soft':
-            softprune(self.network)
-        else:
-            combination(self.network)
+        self.values = []
+        for parameter in self.Pruning.model.parameters():
+            if len(parameter.data.size()) == 4: # Select convolution layers
+                parameter_np = parameter.data.cpu().numpy()
+                values_this_layer = np.square(parameter_np).sum(axis=1).sum(axis=1).sum(axis=1)\
+                    /(parameter_np.shape[1]*parameter_np.shape[2]*parameter_np.shape[3]) # Sum of squared values divided by the amount of values
+            
+                # normalization (important)
+                values_this_layer = values_this_layer / np.sqrt(np.square(values_this_layer).sum())
+                min_value, min_ind = arg_nonzero_min(list(values_this_layer))
+                self.values.append([min_value, min_ind])
+                #print('min value:',min_value,'min ind', min_ind)
+        self.values = np.array(self.values)
+        layer_index = np.argmin(self.values[:, 0])
+        filter_index = int(self.values[layer_index, 1])
+
+        #count = 0
+        #for x in self.Pruning.convolutionlayerlist:
+        #    print('convolutielaag', count, 'heeft', self.Pruning.model.layers[x[0]][x[1]].out_channels, 'filters')
+        #    count +=1
+
+        self.pruneFilter(layer_index, filter_index)
+        self.prunedfilters += 1
+
+        while self.prunedfilters < 20: #self.pruneamount:
+            printProgressBar(self.prunedfilters, self.pruneamount)
+            layer_index, filter_index = self.findFilter()
+            self.pruneFilter(layer_index, filter_index)
+            self.prunedfilters += 1   
+
+
+    def findFilter(self):
+        self.values = []
+        for parameter in self.Pruning.model.parameters():
+            if len(parameter.data.size()) == 4: # Select convolution layers
+                parameter_np = parameter.data.cpu().numpy()
+                values_this_layer = np.square(parameter_np).sum(axis=1).sum(axis=1).sum(axis=1)\
+                    /(parameter_np.shape[1]*parameter_np.shape[2]*parameter_np.shape[3]) # Sum of squared values divided by the amount of values
+            
+                # normalization (important)
+                values_this_layer = values_this_layer / np.sqrt(np.square(values_this_layer).sum())
+                min_value, min_ind = arg_nonzero_min(list(values_this_layer))
+                self.values.append([min_value, min_ind])
+                #print('min value:',min_value,'min ind', min_ind)
+        self.values = np.array(self.values)
+        layer_index = np.argmin(self.values[:, 0])
+        filter_index = int(self.values[layer_index, 1])
+
+        return layer_index, filter_index
+
+
+    def pruneFilter(self, layer_index, filter_index):
+        print('Pruning filter',filter_index, '@ layer', layer_index)
+        layer = 0
+        for parameter in self.Pruning.model.parameters():
+            if len(parameter.data.size()) == 4:
+                if layer != layer_index:
+                    layer += 1
+                    continue
+                parameter_np = parameter.data.cpu().numpy()
+                print(parameter_np.shape)
+                parameter_np = np.delete(parameter_np, filter_index, axis=0)
+                print(parameter_np.shape)
+                #Set weights
+                break
         return
-
-    def findtargetlayers(self):
-        return layers
-
-
-if __name__ == '__main__':
-    # Define arguments
-    parser = argparse.ArgumentParser(
-        description='Prune a given network for a given percentage',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-
-    parser.add_argument('loss', help='Allowed loss value', type=float)
-    parser.add_argument('percentage', help='Percentage of network to be pruned', type=float)
-    parser.add_argument('-n', '--network', help='Network to prune',
-                        choices=['Yolo', 'Yolt', 'DYolo', 'TinyYolo', 'MobileNetYolo', 'test'], default='Yolo')
-    parser.add_argument('-m', '--manner', choices=['hard', 'soft', 'combination'], default='hard',
-                        help='The manner in which to prune: soft, hard or a combination of both')
-    parser.add_argument('-o', '--optimizer', help='Optimizer to use')
-    args = parser.parse_args()
-
-    net = getnet(args.network)
-
-    # Start pruning
-    pruning = L2prune(
-        net,
-        loss=args.loss,
-        percentage=args.percentage,
-        manner=args.manner,
-        optimizer=args.optimizer,
-    )
-    pruning()
